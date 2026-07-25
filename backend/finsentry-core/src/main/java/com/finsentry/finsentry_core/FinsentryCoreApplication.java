6d36dbf5c5d14517fa0f3354c6fdced10ca11965
Devashish_204 <devashishrajput09@gmail.com>
2026-07-25 23:23:18 +0530

    chore: setup Spring Boot application with Dockerized PostgreSQL

---
+++ backend/finsentry-core/src/main/java/com/finsentry/finsentry_core/FinsentryCoreApplication.java
@@ -0,0 +1,13 @@
+package com.finsentry.finsentry_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsentryCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinsentryCoreApplication.class, args);
+	}
+
+}
---
+++ backend/finsentry-core/src/test/java/com/finsentry/finsentry_core/FinsentryCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsentry.finsentry_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinsentryCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
